2e6c88df293f56570868fd6c0de15a3f3bef3596
Yuri Leikind <yuri.leikind@gmail.com>
2009-09-17 23:21:26 +0200

    initial ordering conditions, resords per page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :basics2
   map.resources :basics3
   map.resources :basics4
+  map.resources :basics5
   map.resources :tasks
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
