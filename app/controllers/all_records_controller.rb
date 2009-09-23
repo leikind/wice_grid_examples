9c40f54cf8863b371675f989e42458664b4c15f5
Yuri Leikind <yuri.leikind@gmail.com>
2009-09-23 18:13:50 +0200

    hiding all records link

---
+++ app/controllers/all_records_controller.rb
@@ -0,0 +1,21 @@
+class AllRecordsController < ApplicationController
+  
+  def index
+    @tasks_grid = initialize_grid(Task,
+      :include => [:priority, :status, :project, :assigned_users],
+      :order => 'statuses.name',
+      :custom_order => {
+        'tasks.priority_id' => 'priorities.name',
+        'tasks.status_id' => 'statuses.position',
+        'tasks.project_id' => 'projects.name'
+      }
+    )
+  end
+
+  protected
+
+  def setup_ui
+    @current_example_key = :all_records_path
+  end
+
+end
