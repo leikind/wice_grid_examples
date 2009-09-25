8f7b9f33de36a0d0acddcdd56429dbc8528dab42
Yuri Leikind <yuri.leikind@gmail.com>
2009-09-25 20:15:01 +0200

    tests added

---
+++ test/functional/custom_filters2_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class CustomFilters2ControllerTest < ActionController::TestCase
+  
+  uses_rake_task_to_populate_the_database
+  
+  def test_should_get_index
+    get :index
+    assert_response :success
+    # assert_not_nil assigns(:second_level_navigation)
+  end
+
+end
+
---
+++ test/functional/dates_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class DatesControllerTest < ActionController::TestCase
+  
+  uses_rake_task_to_populate_the_database
+  
+  def test_should_get_index
+    get :index
+    assert_response :success
+    # assert_not_nil assigns(:second_level_navigation)
+  end
+
+end
