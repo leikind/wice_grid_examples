40f5777fd77319d67ade71b5f1fdfff8467b69a6
Yuri Leikind <yuri@pogio.net>
2011-02-18 14:22:43 +0100

    added an integer filter to the example with detached filters

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090907214632) do
+ActiveRecord::Schema.define(:version => 20100117201812) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
