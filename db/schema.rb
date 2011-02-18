4974d7dbe8292e9e774ebcf42c02bd73e4a87b9a
Yuri Leikind <yuri@pogio.net>
2011-02-18 14:22:43 +0100

    added an integer filter to the example with detached filters

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101101191752) do
+ActiveRecord::Schema.define(:version => 20100117201812) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "foo_tasks", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
