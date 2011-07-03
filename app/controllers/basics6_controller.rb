3b6cb397ab51f76c8a93186fc2a0737f86d05e2e
Yuri Leikind <yuri.leikind@gmail.com>
2011-07-03 19:02:31 +0200

    using active record relation as a condition

--- app/controllers/basics6_controller.rb
+++ app/controllers/basics6_controller.rb
@@ -2,10 +2,10 @@ class Basics6Controller < ApplicationController
   
   # <example>
   def index
-    @tasks_grid = initialize_grid(Task,
+    @tasks_grid = initialize_grid(Task.where(:archived => false),
      :order => 'tasks.title',
      :order_direction => 'desc',
-     :conditions => ['archived = ?', false],
+     # :conditions => ['archived = ?', false],
      :per_page => 20
     )
   end
